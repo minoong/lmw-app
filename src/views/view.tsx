f4558fe721f8f1a568196365dccf902fa3aec281
lmw <alsdndjdrktl@gmail.com>
2022-01-17 21:12:52 +0900

    feat : d3 legend

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import portfolio from '../static/portfolio.json'
 import schc from '../static/SCHC.json'
 import vcit from '../static/VCIT.json'
+import Legend from './legend/Legend'
 import MultilineChart, { IPortfolio, ISchc, IVcit } from './MultilineChart'
 
 const portfolioData: IPortfolio = {
@@ -16,7 +17,7 @@ const schcData: ISchc = {
  items: schc.map((d) => ({ ...d, date: new Date(d.date) })),
 }
 
-const cvitData: IVcit = {
+const vcitData: IVcit = {
  name: 'VCIT',
  color: '#5e4fa2',
  items: vcit.map((d) => ({ ...d, date: new Date(d.date) })),
@@ -34,9 +35,18 @@ const dimensions = {
 }
 
 const view = () => {
+ const [selectedItems, setSelectedItems] = useState<string[]>([])
+ const legendData = [portfolioData, schcData, vcitData]
+ const chartData = [portfolioData, ...[schcData, vcitData].filter((d) => selectedItems.includes(d.name))]
+
+ const onChangeSelection = (name: string) => {
+  const newSelectedItems = selectedItems.includes(name) ? selectedItems.filter((item) => item !== name) : [...selectedItems, name]
+  setSelectedItems(newSelectedItems)
+ }
  return (
   <div className="bg-gray-900">
-   <MultilineChart data={[portfolioData, schcData, cvitData]} dimensions={dimensions} />
+   <Legend data={legendData} selectedItems={selectedItems} onChangeSelection={onChangeSelection} />
+   <MultilineChart data={chartData} dimensions={dimensions} />
   </div>
  )
 }
